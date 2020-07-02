ce76ad38c41ed0c419bebb9adef61cb73d61c416
Nisha Choudhary <choudharynisha024@gmail.com>
2020-07-01 22:18:14 -0400

    New Expo App Created

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+import { Button } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <Text>Welcome to my FIRST React Native App!</Text>
+      <Button title = 'My First Button' icon = {{ name: 'code'}} />
+      <StatusBar style = "auto" />
     </View>
   );
 }
